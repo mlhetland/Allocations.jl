0f91db2c29e1987a8ae1d7fa74910e782f871ecf
zootos <halvard.hummel@hotmail.com>
2021-01-07 09:55:40 +0100

    Name simplification and MMS reduction generalization

--- src/algorithms.jl
+++ src/algorithms.jl
@@ -4,62 +4,30 @@
 Find a 1/2-approximate MMS allocation that obeys the constraints imposed by C.
 """
 function alloc_half_mms(V::Additive, C::Counts)
-    V, C, convert = create_ordered_instance(V, C)
-    return convert(alloc_half_mms_big_item_reduction(V, C))
+    V, C, convert = order(V, C)
+	V, C, convert2 = reduce(V, C, 0.5)
+	return convert(convert2(alloc_half_mms_bag_filling(V, C)))
 end
 
 
-"""
-    alloc_half_mms_big_item_reduction(V::Additive, C::Vector{OrderedCategory})
-
-First part of `half_mms`. Takes an ordered instance, normalizes the values and
-gives any agent that value an individual item greater than or equal to 1/2 the
-item and any low value items required to reduce to a valid instance. After
-performing this reduction, the function calls itself recursively. If there is
-no such agent-item pairs, `alloc_half_mms_bag_filling` is called.
-"""
-function alloc_half_mms_big_item_reduction(V::Additive, C::Vector{OrderedCategory})
-    N, n, M = agents(V), na(V), items(V)
-
-    if n == 1
-        A = Allocation(1, ni(V))
-        for j in M
-            give!(A, 1, j)
-        end
-        return A
-    end
-
-    V = normalize(V)
-
-    # Find any agent who values a single item at least 0.5 and give that item
-    # to the agent along with the least valuable items in the remaining bundles
-    # that must be given to the agent to guarantee a valid instance.
-    for i in N, j in M
-		if value(V, i, j) >= 0.5
-			bundle = union(Set{Int}(j), [c[end - required(c, n) + (j in c) + 1:end] for c in C]...)
-			V, C, convert = reduce_instance(V, C, i, bundle)
-			return convert(alloc_half_mms_big_item_reduction(V, C))
-		end
-    end
-
-    return alloc_half_mms_bag_filling(V, C)
-end
 
 
 """
 	alloc_half_mms_bag_filling(V::Additive, C::Vector{OrderedCategory})
 
-The second part of `half_mms`. Takes a normalized ordered instance without any
-items worth 1/2 or more to any agent and produces a valid
-1/2-MMS allocation. The algorithm creates a bundle of the ⌊length(category)/n⌋
-lowest-valued items in each category. Repeatedly, it converts each of these to
-the highest-valued remaining item in the category until it either runs out of
-items to convert or an agent values the bundle at least 1/2. If the procedure
-runs out of items to convert, it adds the highest-valued remaining
-item in each category, in order, to get ⌈length(category)/n⌉ items from each category.
-After each such item is added, the value is again checked for each agent. As
-long as the initial ordered instance was normalized, the procedure will never
-run out of items to add and have no agent value the bundle at least 1/2.
+Takes a normalized ordered instance without any items worth 1/2 or more to any
+agent and produces a valid 1/2-MMS allocation. The algorithm creates a bundle
+of the ``⌊length(category)/n⌋`` lowest-valued items in each category.
+Repeatedly, it converts each of these to the highest-valued remaining item in
+the category until it either runs out of items to convert or an agent values
+the bundle at least 1/2. If the procedure runs out of items to convert, it adds
+the highest-valued remaining item in each category, in order, to get
+``⌈length(category)/n⌉`` items from each category. After each such item is
+added, the value is again checked for each agent. As long as the initial
+ordered instance was normalized and without items worth 1/2 or more, the bundle
+created will always be worth more than 1/2 to one of the remaining agents
+before the procedure runs out of items to add to it or convert from low- to 
+high-valued.
 """
 function alloc_half_mms_bag_filling(V::Additive, C::Vector{OrderedCategory})
     N, n = agents(V), na(V)
@@ -103,6 +71,6 @@ function alloc_half_mms_bag_filling(V::Additive, C::Vector{OrderedCategory})
         end
     end
 
-    V, C, convert = reduce_instance(V, C, findfirst(i -> value(V, i, bundle) >= 0.5, N), bundle)
+    V, C, convert = reduce(V, C, findfirst(i -> value(V, i, bundle) >= 0.5, N), bundle)
     return convert(alloc_half_mms_bag_filling(V, C))
 end
--- src/reductions.jl
+++ src/reductions.jl
@@ -1,5 +1,5 @@
 """
-    reduce_instance(V::Additive, C::Vector{OrderedCategory}, agent, removedbundle)
+    reduce(V::Additive, C::Vector{OrderedCategory}, agent, removedbundle)
 
 Reduce the instance given by the pair (V, C) to a new instance by giving the
 supplied agent the supplied bundle. Returns an additive weight matrix, a set of
@@ -7,7 +7,7 @@ ordered categories for the new reduced instance and a function that turns an
 allocation in the reduced instance into one for the original instance,
 including giving the supplied agent the supplied bundle.
 """
-function reduce_instance(V::Additive, C::Vector{OrderedCategory}, agent::Int, removedbundle::Set{Int})
+function reduce(V::Additive, C::Vector{OrderedCategory}, agent::Int, removedbundle::Set{Int})
     N, M = agents(V), items(V)
     n, m = na(V), ni(V)
 
@@ -31,23 +31,23 @@ function reduce_instance(V::Additive, C::Vector{OrderedCategory}, agent::Int, re
     # Create new ordered categories
     Cs = OrderedCategory[]
     index = 1
-    for category in C
-        newlength = length(category ∩ translate)
-        push!(Cs, OrderedCategory(index, newlength, category.threshold))
+    for c in C
+        newlength = length(c ∩ translate)
+        push!(Cs, OrderedCategory(index, newlength, c.threshold))
         index += newlength
     end
 
-    return Additive(Vs), Cs, (allocation) -> revert_instance(translate, agent, removedbundle, allocation)
+    return Additive(Vs), Cs, (A) -> revert(translate, agent, removedbundle, A)
 end
 
 
 """
-    revert_instance(translate::Vector{Int}, agent::Int, removedbundle::Set{Int}, allocation::Array{Set{Int}, 1})
+    revert(translate::Vector{Int}, agent::Int, removedbundle::Set{Int}, A::Allocation)
 
 Convert an allocation for a reduced instance to one for the original instance,
 including giving the removed bundle to the removed agent.
 """
-function revert_instance(translate::Vector{Int}, agent::Int, removedbundle::Set{Int}, A::Allocation)
+function revert(translate::Vector{Int}, agent::Int, removedbundle::Set{Int}, A::Allocation)
     A′ = Allocation(na(A) + 1, ni(A) + length(removedbundle))
     for i in 1:na(A)
         new_i = i + (i >= agent)
@@ -65,7 +65,54 @@ end
 
 
 """
-    create_ordered_instance(V::Additive, C::Counts)
+    reduce(V::Additive, C::Vector{OrderedCategory}, α)
+
+Reduces an ordered instance by normalizing the values and giving any agent that
+value an individual item greater than or equal to α the item and any low value
+items required to reduce to a valid instance. This reduction is performed
+recursively until no more such items exist. The reduction does not decrease the
+MMS guarantee of any remaining agents and all agents that are allocated a
+bundle in the reduction is guaranteed to value their bundle at least α of their
+MMS guarantee.
+"""
+function reduce(V::Additive, C::Vector{OrderedCategory}, α::Float64)
+    N, n, M = agents(V), na(V), items(V)
+
+    if n == 1
+        A = Allocation(1, ni(V))
+        for j in M
+            give!(A, 1, j)
+        end
+        return A
+    end
+
+    V = normalize(V)
+
+    # Find any agent who values a single item at least α and give that item to
+	# the agent along with the least valuable items in the remaining bundles
+    # that must be given to the agent to guarantee a valid instance.
+    for i in N, j in M
+		if value(V, i, j) >= α
+			bundle = union(
+				Set{Int}(j),
+				[c[end - required(c, n) + (j in c) + 1:end] for c in C]...)
+			V, C, convert = reduce(V, C, i, bundle)
+
+			# Recursive application, as the removed items may cause items worth
+			# less than α to be worth α or more after a new normalization.
+			V, C, prev_convert = reduce(V, C, alpha)
+
+			# The converters have to be applied in reverse.
+			return V, C, (A) -> convert(prev_convert(A))
+		end
+    end
+
+	return V, C, (A) -> A
+end
+
+
+"""
+    order(V::Additive, C::Counts)
 
 Create an ordered instance for the given weights and categories. The items are
 reorded such that each category has a continous range of indices for its items.
@@ -73,31 +120,31 @@ Returns new additive valutations, an array of `OrderedCategory` objects and
 a function that converts an allocation in the ordered instance to one for the
 original instance.
 """
-function create_ordered_instance(V::Additive, C::Counts)
+function order(V::Additive, C::Counts)
     N = agents(V)
     Vo = zeros(na(V), ni(V))
     Co = OrderedCategory[]
 
     itemcounter = 1
-    for category in C
-         for i in N
-            Vo[i,itemcounter:itemcounter+length(category)-1] = sort([value(V, i, j) for j in category], rev=true)
+    for c in C
+        for i in N
+            Vo[i,itemcounter:itemcounter+length(c)-1] = sort([value(V, i, j) for j in c], rev=true)
         end
 
-        push!(Co, OrderedCategory(itemcounter, length(category), threshold(category)))
-        itemcounter += length(category)
+        push!(Co, OrderedCategory(itemcounter, length(c), threshold(c)))
+        itemcounter += length(c)
     end
 
-    return Additive(Vo), Co, (A) -> revert_to_non_ordered_instance(V, C, Co, A)
+    return Additive(Vo), Co, (A) -> revert(V, C, Co, A)
 end
 
 
 """
-    revert_to_non_ordered_instance(V::Additive, C::Counts, Co::Vector{OrderedCategory}, alloc::Array{Set{Int}})
+    revert(V::Additive, C::Counts, Co::Vector{OrderedCategory}, A:Allocation)
 
 Convert an allocation for the ordered instance to one for the original instance.
 """
-function revert_to_non_ordered_instance(V::Additive, C::Counts, Co::Vector{OrderedCategory}, A::Allocation)
+function revert(V::Additive, C::Counts, Co::Vector{OrderedCategory}, A::Allocation)
     A′ = Allocation(na(A), ni(A))
  
     for (orig, new) in zip(C, Co)
