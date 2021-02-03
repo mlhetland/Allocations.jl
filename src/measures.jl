240ebdc131f0abd5cbc11f47fa66ea6c9379b64e
Magnus Lie Hetland <magnus@hetland.org>
2021-02-03 18:02:48 +0100

    Change nash_welfare to always be Float64

--- src/measures.jl
+++ src/measures.jl
@@ -4,12 +4,14 @@
 Compute the Nash welfare of the allocation `A`, given the valuation `V`, i.e.,
 the product of the individual agent utilities resulting from `A`. The
 `nonzero` keyword indicates that agents with a utility of zero are left out.
-If no agents with nonzero utility exist, the result os zero.
+If no agents with nonzero utility exist, the result is zero. To avoid overflow
+with large utilities, the product is performed using floating-point arithmetic,
+even if the utilities are integral.
 """
 function nash_welfare(V, A; nonzero=true)
-    x = [value(V, i, bundle(A, i)) for i in agents(V)]
+    x = Float64[value(V, i, bundle(A, i)) for i in agents(V)]
     if nonzero
-        x = x[x .> 0]
+        x = x[x .> 0.0]
     end
     return isempty(x) ? 0.0 : prod(x)
 end
