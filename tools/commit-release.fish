61b31ed5863c52ef07587aac6c29137328a44729
Magnus Lie Hetland <magnus@hetland.org>
2022-11-30 18:45:34 +0100

    Add check for `yq` in `commit-release.fish`

--- tools/commit-release.fish
+++ tools/commit-release.fish
@@ -1,13 +1,16 @@
 #!/usr/bin/env fish
 
-# https://github.com/mikefarah/yq
-set VERSION (yq .version CITATION.cff)
+if type -q yq
+    set VERSION (yq .version CITATION.cff)
 
-git add LICENSE.txt
-git add CITATION.cff
-git add Project.toml
+    git add LICENSE.txt
+    git add CITATION.cff
+    git add Project.toml
 
-git commit -m "Change version to $VERSION"
-git tag -a "v$VERSION" -m "Version $VERSION"
+    git commit -m "Change version to $VERSION"
+    git tag -a "v$VERSION" -m "Version $VERSION"
 
-echo "Version $VERSION committed and tagged (but not pushed)"
+    echo "Version $VERSION committed and tagged (but not pushed)"
+else
+    echo "Install yq (from https://github.com/mikefarah/yq)"
+end
