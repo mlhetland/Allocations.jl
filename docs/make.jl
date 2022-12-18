e8f483df019ab3b421ef0a28089e33e42a1b8350
Magnus Lie Hetland <magnus@hetland.org>
2022-12-18 16:59:06 +0100

    Add call to `deploydocs`

--- docs/make.jl
+++ docs/make.jl
@@ -20,9 +20,6 @@ makedocs(
     modules = [Allocations]
 )
 
-# Documenter can also automatically deploy documentation to gh-pages.
-# See "Hosting Documentation" and deploydocs() in the Documenter manual
-# for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+deploydocs(
+    repo = "github.com/" * ENV["GITHUB_REPOSITORY"] * ".git"
+)
